4de7cca26ed2515a83bc0c74194b82451c15924a
EgorAndrik <egorandreas28@gmail.com>
2023-09-04 15:32:43 +0700

    update

--- serverSock/buildgraph.py
+++ serverSock/buildgraph.py
@@ -8,19 +8,19 @@ class GraphBuil:
     def __init__(self):
         self.dataElemInDir = Search_and_save_graph_in_Dir('graphDir')
 
-    def _handler(self, arifmEx):
-        arifmEx = arifmEx.replace(' ', '')
+    def _handler(self, function: str) -> str:
+        arifmEx = function
         if '^' in arifmEx:
             arifmEx = arifmEx.replace('^', '**')
         if len(arifmEx[:arifmEx.find('x**')]) == 0:
             arifmEx = "1*" + arifmEx
         return arifmEx
 
-    def _check(self, arifmEx):
-        varsFunc = ['sin', 'cos', 'sqrt']
-        return any([i in arifmEx for i in varsFunc])
+    def _check(self, function: str) -> bool:
+        varsFunc = ['cos', 'sin', 'abs', 'sqrt', 'tan', 'ctg']
+        return any([i in function for i in varsFunc])
 
-    def buildG(self, arithmetic_example):
+    def buildG(self, arithmetic_example: str):
         function = arithmetic_example
         function = self._handler(function)
         fig, ax = plt.subplots()
@@ -29,34 +29,36 @@ class GraphBuil:
         ax.set_ylabel('y')
         plt.grid(True)
         if not self._check(function):
-            n = []
+            roots = []
 
             if '/' in function:
                 arifmEx = function.split('/')
                 x = symbols('x', real=True)
-                n = solve(Eq(eval(arifmEx[0]), 0), x) + solve(Eq(eval(arifmEx[1]), 0), x)
+                roots = solve(Eq(eval(arifmEx[0]), 0), x) + solve(Eq(eval(arifmEx[1]), 0), x)
 
             if '/' not in function:
                 x = symbols('x', real=True)
-                n = solve(Eq(eval(function), 0), x)
+                roots = solve(Eq(eval(function), 0), x)
 
-                if len(n) == 0 and 'x**2' in function and 'x' in function[function.find('x**') + 1:]:
+                if len(roots) == 0 and 'x**2' in function and 'x' in function[function.find('x**') + 1:]:
                     terms = list(map(lambda x: x.replace('**2', '').replace('*', ''), function.split('x')))
                     a = float(terms[0])
                     b = float(terms[1])
-                    n.append(-b / (2 * a))
+                    roots.append(-b / (2 * a))
 
-            if len(n) == 0:
-                n = [0]
+            if len(roots) == 0:
+                roots = [0]
 
-            elif len(n) > 0 and \
+            elif len(roots) > 0 and \
                     any(([
-                        str(type(i)) in ["<class 'sympy.core.numbers.NegativeOne'>", "<class 'sympy.core.add.Add'>"]
-                        for i in n])):
-                n = [0]
+                        str(type(i)) in ["<class 'sympy.core.numbers.NegativeOne'>", "<class 'sympy.core.add.Add'>",
+                                         "<class 'sympy.core.power.Pow'>", "<class 'sympy.core.mul.Mul'>",
+                                         "<class 'sympy.core.numbers.Rational'>"]
+                        for i in roots])):
+                roots = [0]
 
-            X_min = float(min(n) - 4.0)
-            X_max = float(max(n) + 4.0)
+            X_min = float(min(roots) - 4.0)
+            X_max = float(max(roots) + 4.0)
 
             x = np.linspace(X_min, X_max, 100)
             y = eval(function)
@@ -65,22 +67,9 @@ class GraphBuil:
             fig.savefig(f'graphDir/Graph.png')
 
         else:
-            if 'cos' in function or 'sin' in function:
-                function = '1*' + function
+            function = eval(f"lambda x: {function}")
+            x = np.linspace(0, 4 * np.pi, 100)
+            y = [function(i) for i in x]
+            ax.plot(x, y)
 
-                x = np.linspace(0, 4 * np.pi, 100)
-                y = eval(function[:function.find('sin') - 1]) * \
-                    np.sin(eval(function[function.find('sin') + 4:function.find(')')])) if 'sin' in function \
-                    else eval(function[:function.find('cos') - 1]) * \
-                         np.cos(eval(function[function.find('cos') + 4:function.find(')')]))
-                ax.plot(x, y)
-
-                fig.savefig(f'graphDir/Graph.png')
-            else:
-                functioSqrt = function[function.index('sqrt') + 5:function.find(')')]
-
-                x = np.linspace(0, 9, 100)
-                y = eval(functioSqrt) ** 0.5
-                ax.plot(x, y)
-
-                fig.savefig(f'graphDir/Graph.png')
+            fig.savefig(f'graphDir/Graph.png')
